393b4dcce9ee4dda5e016f1b046b30f84a2159d6
TYerzatuly <tyerzatuly@one.kz>
2022-04-04 23:44:26 +0600

    updates to deploy

--- src/app/credit-calculator/credit-calculator.component.ts
+++ src/app/credit-calculator/credit-calculator.component.ts
@@ -65,7 +65,7 @@ export class CreditCalculatorComponent implements OnInit {
     },1000);
   }
   calculateCredit(){
-    const p = this.interestRate/12;
-    this.monthPayment = Math.ceil(this.amount*(p*(1+p)^this.amountMonth)/((1+p)^this.amountMonth -1));
+    const p = Math.floor((this.interestRate/12)/100);
+    this.monthPayment = Math.ceil(this.amount*((p*(1+p)^this.amountMonth)/((1+p)^this.amountMonth -1)));
   }
 }
