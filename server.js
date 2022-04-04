6a85c4c2941cd6b5ca0927e1f8f9b6e1c94df4a9
TYerzatuly <tyerzatuly@one.kz>
2022-04-04 23:10:31 +0600

    updates to deploy

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-root>'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/<app-root>/index.html'));});
+
+app.use(requireHTTPS);
+app.use(express.static('./dist/test-project'));
+
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/test-project/'}),
+);
+
 app.listen(process.env.PORT || 8080);
